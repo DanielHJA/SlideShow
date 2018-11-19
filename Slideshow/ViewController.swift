456e30885092b19f07e6cd093f2038a097d8d43a
DanielHJA <daniel.hjartstrom@gmail.com>
2018-11-19 10:32:16 +0100

    Initial Commit

---
+++ Slideshow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slideshow
+//
+//  Created by Daniel Hjärtström on 2018-11-19.
+//  Copyright © 2018 Sog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
